1d51ac3421b719fb5603890fad1c59f42e92aac3
Ricky Robinett <ricky@Rickys-MacBook-Pro.local>
2023-11-29 09:23:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Reacting to content with GPT-4V, OpenAI tts, Cloudflare Workers and Mac shortcuts
+
+This repo shows you how to create an application on Cloudflare Workers that lets you have GPT-4V react to anything you are doing on your computer in real-time.
+
+In order to run this application you need a:
+- Cloudflare Account: This app can be run using the Workers free tier.[Sign up for free here](https://dash.cloudflare.com/sign-up).
+- OpenAI Account and API Key: You can [Sign up for an account here](https://openai.com/) and [generate an API key here](https://platform.openai.com/api-keys)
+- Mac or iOS device
+
+# Set up (Mac Shortcuts)
+
+This application works by running a Mac Shortcut that takes the following actions:
+1) Takes a screenshot using the "Take screenshot" action
+2) Resizes that screenshot to be smaller using the "Resize" action
+3) Sends the resized screenshot to our Worker using the "Get contents of" action
+4) Plays the sound returned by our Worker using the "Play sound" action
+
+To do this you can create a new Shortcut called "GPTReact" and copy the Shortcut configuration in this screenshot:
+
+
+# Set up (Cloudflare Workeres)
+
+Clone this repo and then run:
+```
+npm install
+```
+
+After installing your dependencies you'll need to add your OpenAI API key as an environmental variable so we can use it to make our requests to OpenAI:
+```
+npx wrangler secret put OPENAI_API_KEY
+```
+
+With your secret set, you can run this application locally to try it out:
+```
+npm run dev
+```
+
+Copy and paste your local URL into the "Get contents of" action in your Shorcut and then run the shortcut. It will take about 10-15 seconds for GPT-V4 to generate a response to your image and then to use OpenAI's tts to create the audio to have that response spoken back to you.
+
+Once you're done developing you can deploy your application with this command:
+```
+npm run deploy
+```
+
+After your application is deployed, update the URL in the "Get contents of" action in your shortcut.
---
+++ src/index.ts
@@ -0,0 +1,92 @@
+import { Hono } from 'hono'
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from "openai/resources/chat";
+
+type Bindings = {
+    OPENAI_API_KEY: string
+  }
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+const systemPrompt: ChatCompletionMessageParam[] = [
+    {
+        role: "system",
+        content: `You are Artificial Intelligence (GPT) receiving images and reacting to them in the style of YouTube reactions. Be funny.`
+    }
+];
+
+let messages: ChatCompletionMessageParam[] = systemPrompt;
+
+app.get('/reset', async (c) => {
+    messages = systemPrompt;
+    c.render("Reset complete");
+});
+
+app.post('/react', async (c) => {
+    const openai = new OpenAI({
+        apiKey:  c.env.OPENAI_API_KEY
+    });
+
+    const blob = await c.req.raw.blob();
+
+    const arrayBuffer = await blob.arrayBuffer();
+    const bytes = new Uint8Array(arrayBuffer);
+    let binary = '';
+    for (let i = 0; i < bytes.byteLength; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }    
+
+    const base64 = "data:image/png;base64," + btoa(binary);
+    
+    messages.push({
+        role: "user",
+        content: [
+            { type: "text", text: `Here's a part of the video. Give your reaction` },
+            {
+                type: "image_url",
+                image_url: {
+                    "url": base64,
+                },
+            },
+        ],
+    }) 
+
+    console.log("making request");
+
+    const response = await openai.chat.completions.create({
+        messages: messages,
+        model: "gpt-4-vision-preview",
+        max_tokens: 4096
+    });
+
+    console.log("request complete");
+
+    console.log(response.choices[0].message.content);
+
+    messages.push({
+        role: "assistant",
+        content: response.choices[0].message.content
+    }) 
+
+    console.log("tts start");
+
+    const input = response.choices[0].message.content || "";
+    const mp3 = await openai.audio.speech.create({
+        model: "tts-1",
+        voice: "alloy",
+        input: input,
+    });
+    
+    const audioBuffer = await mp3.arrayBuffer();
+
+    console.log("tts complete");
+
+    // Set appropriate headers
+    c.header('Content-Type', 'audio/mpeg');
+    c.header('Content-Length', audioBuffer.byteLength.toString());
+
+    return c.body(audioBuffer);
+
+  })
+
+export default app
